beed74b97fc2a1669e8921f6450201b1d7d42699
Cody Chen <codychen1@gmail.com>
2020-11-18 14:34:11 -0700

    added prompt user function with inquirer

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 const inquirer = require("inquirer");
 const fs = "fs";
 const util = require("util");
+
+const promptUser = () =>
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the name of your project?",
+    },
+  ]);
